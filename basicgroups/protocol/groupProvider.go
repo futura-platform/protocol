6f183aceb3e212d2a8c428d1c64f012455e0721a
nil <tomeaton76@gmail.com>
2024-11-15 13:36:02 -0800

    Update groupProvider.go

--- basicgroups/protocol/groupProvider.go
+++ basicgroups/protocol/groupProvider.go
@@ -6,4 +6,6 @@ type Provider[T Parsable[T]] interface {
 	LoadEntry(ctx context.Context) (T, error)
 	ReplaceEntry(ctx context.Context, old T, new T) error
 	CheckEntryExistence(ctx context.Context, entry T) (bool, error)
+
+	GetEntries(ctx context.Context) ([]T, error)
 }
