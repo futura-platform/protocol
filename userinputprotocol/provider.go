e0a04ad8cf8bb92a61bafd4ad49cf594ac66efb5
nil <tomeaton76@gmail.com>
2024-11-19 20:26:50 -0800

    Update provider.go

--- userinputprotocol/provider.go
+++ userinputprotocol/provider.go
@@ -16,7 +16,7 @@ type InputField struct {
 }
 
 type Provider interface {
-	GetUserFormInput(ctx flowprotocol.Context, typeId string, details map[string]string, inputs []InputField) (UserFormInput, error)
+	GetUserFormInput(ctx flowprotocol.Context, typeId, imageUrl string, details map[string]string, inputs []InputField) (UserFormInput, error)
 }
 
 type UserFormInput interface {
