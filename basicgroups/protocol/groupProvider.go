2d0e29fd6443631129a96ca9224dd9f934404219
nil <tomeaton76@gmail.com>
2025-02-16 10:14:26 -0800

    Update groupProvider.go

--- basicgroups/protocol/groupProvider.go
+++ basicgroups/protocol/groupProvider.go
@@ -3,7 +3,11 @@ package basicgroupsprotocol
 import "context"
 
 type Provider[T Parsable[T]] interface {
-	LoadEntry(ctx context.Context) (T, error)
+	// Returns an entry specific to the current task, that won't change between calls
+	GetStaticEntry(ctx context.Context) (T, error)
+	// Atomically loads entries on a global increment. 1st call will return the first entry, 2nd call will return the 2nd entry, etc.
+	LoadIncrementalEntry(ctx context.Context) (T, error)
+
 	ReplaceEntry(ctx context.Context, old T, new T) error
 	CheckEntryExistence(ctx context.Context, entry T) (bool, error)
 
