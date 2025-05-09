db53b1154f3f980157e45f2787ad0dc555cf948e
nil <tomeaton76@gmail.com>
2025-05-09 12:35:58 -0700

    Update settings.go

--- settingsprotocol/settings.go
+++ settingsprotocol/settings.go
@@ -5,19 +5,20 @@ import (
 )
 
 type UserSettings struct {
-	WebhookUrl         string `json:"webhookUrl"`
-	QueueWebhookUrl    string `json:"queueWebhookUrl"`
-	CapMonsterApiKey   string `json:"capMonsterApiKey"`
-	TwoCapApiKey       string `json:"twoCapApiKey"`
-	EZCaptchaApiKey    string `json:"ezCaptchaApiKey"`
-	EnigmaSolverApiKey string `json:"enigmaSolverApiKey"`
-	CapSolverApiKey    string `json:"capSolverApiKey"`
-	AntiCaptchaApiKey  string `json:"antiCaptchaApiKey"`
-	TextVerifiedApiKey string
-	SMSPoolApiKey      string
-	PVADealsApiKey     string
-	MajorPhonesLogin   string
-	DaisySMSApiKey     string
+	WebhookUrl              string `json:"webhookUrl"`
+	QueueWebhookUrl         string `json:"queueWebhookUrl"`
+	CapMonsterApiKey        string `json:"capMonsterApiKey"`
+	TwoCapApiKey            string `json:"twoCapApiKey"`
+	EZCaptchaApiKey         string `json:"ezCaptchaApiKey"`
+	EnigmaSolverApiKey      string `json:"enigmaSolverApiKey"`
+	CapSolverApiKey         string `json:"capSolverApiKey"`
+	AntiCaptchaApiKey       string `json:"antiCaptchaApiKey"`
+	FreeCaptchaBypassApiKey string
+	TextVerifiedApiKey      string
+	SMSPoolApiKey           string
+	PVADealsApiKey          string
+	MajorPhonesLogin        string
+	DaisySMSApiKey          string
 }
 
 type Provider interface {
