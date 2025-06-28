7a26a7d0135299a86c727ef2da1167ffebc43c8c
nil <tomeaton76@gmail.com>
2025-06-27 17:21:45 -0700

    Update browserProfiles.go

--- netprotocol/browserProfiles.go
+++ netprotocol/browserProfiles.go
@@ -158,7 +158,20 @@ var (
 		Mobile:          false,
 		Bitness:         "64",
 	}
-	ChromeLatest_mac = Chrome137_mac
+	Chrome138_mac = BrowserProfile{
+		UserAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+
+		Brands:          `"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"`,
+		FullVersionList: `"Not)A;Brand";v="8.0.0.0", "Chromium";v="138.0.7204.49", "Google Chrome";v="138.0.7204.49"`,
+
+		Platform:        `macOS`,
+		PlatformVersion: "138.0.7204.49",
+		Architecture:    "arm",
+		Model:           "",
+		Mobile:          false,
+		Bitness:         "64",
+	}
+	ChromeLatest_mac = Chrome138_mac
 
 	Chrome127_windows = BrowserProfile{
 		UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
