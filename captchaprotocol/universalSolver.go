d1a9e791b59c9e242cc09dcf04ae5e0fa4c7d024
nil <tomeaton76@gmail.com>
2025-07-18 17:40:17 -0400

    Update universalSolver.go

--- captchaprotocol/universalSolver.go
+++ captchaprotocol/universalSolver.go
@@ -10,13 +10,13 @@ type CaptchaParams interface {
 }
 
 type Solver interface {
-	SolveRecaptchaV2(ctx context.Context, params RecaptchaV2Params, silent bool) (solution string, delay time.Duration, reportFailure func() error, err error)
-	SolveRecaptchaV3(ctx context.Context, params RecaptchaV3Params, silent bool) (solution string, delay time.Duration, reportFailure func() error, err error)
-	SolveHcaptcha(ctx context.Context, params HcaptchaParams, silent bool) (solution string, delay time.Duration, reportFailure func() error, err error)
-	SolveTurnstile(ctx context.Context, params TurnstileParams, silent bool) (solution string, delay time.Duration, reportFailure func() error, err error)
-	SolveImageToText(ctx context.Context, params ImageToTextParams, silent bool) (solution string, delay time.Duration, reportFailure func() error, err error)
-	SolveDatadome(ctx context.Context, params DatadomeParams, silent bool) (solution string, delay time.Duration, reportFailure func() error, err error)
-	SolveAWSWAFToken(ctx context.Context, params AWSWAFTokenParams, silent bool) (solution string, delay time.Duration, reportFailure func() error, err error)
-	SolveGeetestV3(ctx context.Context, params GeetestParamsV3, silent bool) (solution GeetestSolutionV3, delay time.Duration, reportFailure func() error, err error)
-	SolveGeetestV4(ctx context.Context, params GeetestParamsV4, silent bool) (solution GeetestSolutionV4, delay time.Duration, reportFailure func() error, err error)
+	SolveRecaptchaV2(ctx context.Context, params RecaptchaV2Params, silent bool) (solution string, delay time.Duration, reportFailure func(context.Context) error, err error)
+	SolveRecaptchaV3(ctx context.Context, params RecaptchaV3Params, silent bool) (solution string, delay time.Duration, reportFailure func(context.Context) error, err error)
+	SolveHcaptcha(ctx context.Context, params HcaptchaParams, silent bool) (solution string, delay time.Duration, reportFailure func(context.Context) error, err error)
+	SolveTurnstile(ctx context.Context, params TurnstileParams, silent bool) (solution string, delay time.Duration, reportFailure func(context.Context) error, err error)
+	SolveImageToText(ctx context.Context, params ImageToTextParams, silent bool) (solution string, delay time.Duration, reportFailure func(context.Context) error, err error)
+	SolveDatadome(ctx context.Context, params DatadomeParams, silent bool) (solution string, delay time.Duration, reportFailure func(context.Context) error, err error)
+	SolveAWSWAFToken(ctx context.Context, params AWSWAFTokenParams, silent bool) (solution string, delay time.Duration, reportFailure func(context.Context) error, err error)
+	SolveGeetestV3(ctx context.Context, params GeetestParamsV3, silent bool) (solution GeetestSolutionV3, delay time.Duration, reportFailure func(context.Context) error, err error)
+	SolveGeetestV4(ctx context.Context, params GeetestParamsV4, silent bool) (solution GeetestSolutionV4, delay time.Duration, reportFailure func(context.Context) error, err error)
 }
