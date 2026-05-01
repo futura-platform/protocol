37924879f2dfa77ccf5d82b0181eeb4e65694650
nil <tomeaton76@gmail.com>
2026-04-30 19:53:10 -0700

    Update browserProfiles.go

--- netprotocol/browserProfiles.go
+++ netprotocol/browserProfiles.go
@@ -262,7 +262,20 @@ var (
 		Mobile:          false,
 		Bitness:         "64",
 	}
-	ChromeLatest_mac = Chrome145_mac
+	Chrome147_mac = BrowserProfile{
+		UserAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
+
+		Brands:          `"Google Chrome";v="147", "Not.A/Brand";v="8", "Chromium";v="147"`,
+		FullVersionList: `"Google Chrome";v="147.0.7727.102", "Not.A/Brand";v="8.0.0.0", "Chromium";v="147.0.7727.102"`,
+
+		Platform:        `macOS`,
+		PlatformVersion: "147.0.7727.102",
+		Architecture:    "arm",
+		Model:           "",
+		Mobile:          false,
+		Bitness:         "64",
+	}
+	ChromeLatest_mac = Chrome147_mac
 
 	Chrome127_windows = BrowserProfile{
 		UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
@@ -394,7 +407,20 @@ var (
 		Mobile:          false,
 		Bitness:         "64",
 	}
-	ChromeLatest_windows = Chrome145_windows
+	Chrome147_windows = BrowserProfile{
+		UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
+
+		Brands:          `"Google Chrome";v="147", "Not.A/Brand";v="8", "Chromium";v="147"`,
+		FullVersionList: `"Google Chrome";v="147.0.7727.102", "Not.A/Brand";v="8.0.0.0", "Chromium";v="147.0.7727.102"`,
+
+		Platform:        `Windows`,
+		PlatformVersion: "147.0.7727.102",
+		Architecture:    "x86",
+		Model:           "",
+		Mobile:          false,
+		Bitness:         "64",
+	}
+	ChromeLatest_windows = Chrome147_windows
 
 	Chrome127_android = BrowserProfile{
 		UserAgent: "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36",
